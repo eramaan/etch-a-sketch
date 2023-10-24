9744bcf197d3b55e6b34fcce2f211432af3a2529
eramaan <58112194+eramaan@users.noreply.github.com>
2023-10-24 16:46:47 +0200

    fixed some issues, created more

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // DOM access
 let btnCreator = document.querySelector("#creator")
 let btnCleaner = document.querySelector("#cleaner")
+let btnClassic = document.querySelector("#classic")
+let btnRainbow = document.querySelector("#rainbow")
+let btnDarkener = document.querySelector("#darkener")
 let gridContainer = document.querySelector("#grid-container")
+let squares = document.querySelectorAll('.square');
 
 // grid dimension & tiles number
 let gridDimension = 800
@@ -38,46 +42,62 @@ btnCreator.addEventListener('click', () => {
     //disable the #creator button
     btnCreator.disabled = true;
 
-    // // add hovering event
-    // let squares = document.querySelectorAll('.square');
-    //     for (let k = 0; k < squares.length; k++) {
-    //         squares[k].addEventListener("mouseout", () => {
-    //             squares[k].classList.add('checked');
-    //         });
-    //     }
-
-    // // rainbow hovering event
-    // let randomHexColorCode = () => {
-    //     let n = (Math.random() * 0xfffff * 1000000).toString(16);
-    //     return '#' + n.slice(0, 6);
-    //   };
-
-    // let squares = document.querySelectorAll('.square');
-    // for (let k = 0; k < squares.length; k++) {
-    //     squares[k].addEventListener("mouseout", () => {
-    //         let randomColor = randomHexColorCode();
-    //         squares[k].style.backgroundColor = randomColor;
-    //     });
-    // }
-
     // add hovering event
     let squares = document.querySelectorAll('.square');
+        for (let k = 0; k < squares.length; k++) {
+            squares[k].addEventListener("mouseout", () => {
+                squares[k].classList.add('checked');
+                squares[k].style.opacity = 0.1;
+
+            });
+        }
+});
+
+
+// clean grids
+btnCleaner.addEventListener('click', () => {
+    for (let k = 0; k < squares.length; k++) {
+            squares[k].classList.remove('checked');
+            squares[k].style.backgroundColor = "initial";
+            squares[k].style.opacity = "initial";        
+    }
+});
+
+// add hovering event
+btnClassic.addEventListener('click', () => {
     for (let k = 0; k < squares.length; k++) {
         squares[k].addEventListener("mouseout", () => {
             squares[k].classList.add('checked');
-            let computedStyle = window.getComputedStyle(squares[k]);
-            let opacityCurrent = computedStyle.getPropertyValue('opacity');
-            squares[k].style.opacity = parseFloat(opacityCurrent) + 0.1;
-            console.log(opacityCurrent)
         });
     }
-
-    // clean grids
-    btnCleaner.addEventListener('click', () => {
-        for (let k = 0; k < squares.length; k++) {
-                squares[k].classList.remove('checked');
-                squares[k].style.backgroundColor = "initial";    
-        }
-    });
 });
 
+// rainbow hovering event
+btnRainbow.addEventListener('click', () => {
+    let randomHexColorCode = () => {
+        let n = (Math.random() * 0xfffff * 1000000).toString(16);
+        return '#' + n.slice(0, 6);
+    };
+
+    let squares = document.querySelectorAll('.square');
+    for (let k = 0; k < squares.length; k++) {
+        squares[k].addEventListener("mouseout", () => {
+            let randomColor = randomHexColorCode();
+            squares[k].style.backgroundColor = randomColor;
+        });
+    }
+});
+
+// add gray shades event
+btnDarkener.addEventListener('click', () => {
+    let squares = document.querySelectorAll('.square');
+    for (let k = 0; k < squares.length; k++) {
+    squares[k].addEventListener("mouseout", () => {
+        squares[k].classList.add('checked');
+        let computedStyle = window.getComputedStyle(squares[k]);
+        let opacityCurrent = computedStyle.getPropertyValue('opacity');
+        squares[k].style.opacity = parseFloat(opacityCurrent) + 0.1;
+        console.log(opacityCurrent)
+    });
+    }
+});
